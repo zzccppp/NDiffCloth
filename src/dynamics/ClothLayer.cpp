a62fdd4efd402363b568fa1471b02f239524a51d
ZZDirty <muztaga2@gmail.com>
2023-11-18 01:26:32 +0800

    fix bugs 	modified:   src/dynamics/ClothLayer.cpp

--- src/dynamics/ClothLayer.cpp
+++ src/dynamics/ClothLayer.cpp
@@ -2,6 +2,11 @@
 #include "simulation/World.hpp"
 
 dynamics::ClothLayer::ClothLayer(simulation::World *w) {
+    if (w == nullptr) {
+        world = nullptr;
+        h = 0.0;
+        return;
+    }
     world = w;
     h = w->timeStep;
 }
