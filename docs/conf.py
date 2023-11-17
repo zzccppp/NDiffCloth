ad9be94320dbea76acf6a8cac1dfa278c287b3cb
ZZDirty <muztaga2@gmail.com>
2023-11-18 03:17:46 +0800

    add readthedocs.yaml

--- docs/conf.py
+++ docs/conf.py
@@ -55,5 +55,5 @@ highlight_language = 'cpp'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
